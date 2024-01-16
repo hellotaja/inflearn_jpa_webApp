b777795c479f0d2b9d923f592b91af5f748e72ad
noorykwon <noorykwon@gmail.com>
2024-01-16 16:07:00 +0900

    section 5 상품 도메인 개발

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional //위에 리드온리로 걸려있으니 오버라이드 되지 않도록 save는 별도로 걸어준다.
+    public void save(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+
+}
